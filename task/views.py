d4b5c8df829a27099f5b049b11d6187fe4231d44
fshahriar298 <mdfahimshahriar17@gmail.com>
2026-05-09 12:38:39 +0600

    created task list function

--- task/views.py
+++ task/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_list_or_404
+from django.contrib.auth.decorators import login_required
+from .models import Task
+from .forms import TaskForm
 
-# Create your views here.
+
+@login_required
+def task_list(request):
+    status_filter = request.GET.get('status', 'all')
+    category_filter = request.GET.get('category', 'all')
+    tasks = Task.objects.filter(user = request.user)
+
+    if status_filter != 'all':
+        tasks = tasks.filter(is_completed=(status_filter=='completed'))
+
+    if category_filter != 'all':
+        tasks = tasks.filter(category=category_filter)
+
+    completed_tasks = tasks.filter(is_completed = True)
+    pending_tasks = tasks.filter(is_completed = False)
+
+    return render(request, '', {
+        'completed_tasks' : completed_tasks,
+        'pending_tasks' : pending_tasks,
+        'status_filter' : status_filter,
+        'category_filter' : category_filter,
+    })
